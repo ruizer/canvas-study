50a601cb3bbc63bda2a3694087b1cd72e24371a9
ruizer <1244388148@qq.com>
2020-12-29 15:00:13 +0800

    favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "canvas动画",
     description:
         "如何借助 Canvas 和数学物理的知识在 Web 上创建交互式动画；基础动画、高级动画、3D 动画；动画的基本概念、动画中的三角学、速度向量和加速度、边界和摩擦力、用户交互、缓动和弹动的原理、碰撞检测、撞球物理、粒子与万有引力、从 2D 到 3D 等等；",
-    head: [["link", { rel: "icon", href: "//images/favicon.png" }]],
+    head: [["link", { rel: "icon", href: "/images/favicon.png" }]],
     markdown: {
         lineNumbers: true,
         anchor: {
